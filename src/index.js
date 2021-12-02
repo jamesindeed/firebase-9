df09eb039fd38c4f9ed93727bc1cfe49ca8cb6af
James Troughton <jamesstroughton@gmail.com>
2021-12-02 14:42:36 +1300

    ✨

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc,
+  deleteDoc,
+  doc,
+  query,
+  getDocs,
+  where,
+  orderBy,
+  serverTimestamp,
+  getDoc,
+  updateDoc,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBm7-SzimCTOo1O9SOM16MLjceP6JtHvM8',
@@ -16,14 +30,72 @@ const db = getFirestore();
 
 const colRef = collection(db, 'books');
 
-getDocs(colRef)
-  .then((snapshot) => {
-    let books = [];
-    snapshot.docs.forEach((doc) => {
-      books.push({ ...doc.data(), id: doc.id });
-    });
-    console.log(books);
-  })
-  .catch((err) => {
-    console.error(err.message);
+const q = query(colRef, orderBy('createdAt'));
+
+// getDocs(colRef)
+//   .then((snapshot) => {
+//     let books = [];
+//     snapshot.docs.forEach((doc) => {
+//       books.push({ ...doc.data(), id: doc.id });
+//     });
+//     console.log(books);
+//   })
+//   .catch((err) => {
+//     console.error(err.message);
+//   });
+
+// *RealTime
+onSnapshot(q, (snapshot) => {
+  let books = [];
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
   });
+  console.log(books);
+});
+
+const addBookForm = document.querySelector('.add');
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+    createdAt: serverTimestamp(),
+  }).then(() => {
+    addBookForm.reset();
+  });
+});
+
+const deleteBookForm = document.querySelector('.delete');
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const docRef = doc(db, 'books', deleteBookForm.id.value);
+
+  deleteDoc(docRef).then(() => {
+    deleteBookForm.reset();
+  });
+});
+
+const docRef = doc(db, 'books', 'zDKT0PeUGH04Tu2gY3HB');
+
+getDoc(docRef).then((doc) => {
+  console.log(doc.data(), doc.id);
+});
+
+onSnapshot(docRef, (doc) => {
+  console.log(doc.data(), doc.id);
+});
+
+const updateForm = document.querySelector('.update');
+updateForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const docRef = doc(db, 'books', updateForm.id.value);
+
+  updateDoc(docRef, {
+    title: 'updated title',
+  }).then(() => {
+    updateForm.reset();
+  });
+});
